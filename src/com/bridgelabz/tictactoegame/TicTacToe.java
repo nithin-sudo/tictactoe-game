403d3e497723fb40df9cb1b2106722d9bd9d3b94
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-25 18:46:58 +0530

    Added:a method and it returnd a sample tictactoe board with empty spaces.

--- src/com/bridgelabz/tictactoegame/TicTacToe.java
+++ src/com/bridgelabz/tictactoegame/TicTacToe.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.tictactoegame;
-
 public class TicTacToe {
+    /**
+     * Printing a welcome message and calling createBoard method.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to tic tac toe game");
+        createBoard();
+    }
+
+    /**
+     * Creating board with empty spaces.
+     * @return
+     */
+    private static char[] createBoard(){
+        char[] board=new char[10];
+        for(int i=1;i<board.length;i++){
+            board[i]='-';
+        }
+        return board;
     }
 }
