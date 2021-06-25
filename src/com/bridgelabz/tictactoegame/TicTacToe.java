a35e90239c3a9798e13ce09ec073dd837611fe52
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-25 19:03:10 +0530

    Added:a method and it will ask user choose between 'x' or 'o'.

--- src/com/bridgelabz/tictactoegame/TicTacToe.java
+++ src/com/bridgelabz/tictactoegame/TicTacToe.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.tictactoegame;
+
+import java.util.Scanner;
+
 public class TicTacToe {
     /**
      * Printing a welcome message and calling createBoard method.
@@ -7,8 +10,8 @@ public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to tic tac toe game");
         createBoard();
+        userOption();
     }
-
     /**
      * Creating board with empty spaces.
      * @return
@@ -19,5 +22,28 @@ public class TicTacToe {
             board[i]='-';
         }
         return board;
+    }
+    /**
+     *asking user to choose between x or o.
+     */
+    private static void userOption(){
+        System.out.println("Choose between x or o : ");
+        Scanner scanner = new Scanner(System.in);
+        char userChoice=scanner.next().charAt(0);
+        char computerChoice;
+        if (userChoice == 'x' || userChoice == 'o') {
+            if (userChoice == userChoice) {
+                computerChoice = 'o';
+                System.out.println("Player has chosen " + userChoice + " and computer is  "+computerChoice);
+            } else {
+                computerChoice = 'x';
+                System.out.println("Player has chosen " + userChoice + " and computer is "+computerChoice);
+            }
+        }
+        else
+        {
+            System.out.println("Enter only x or o");
+            userOption();
+        }
     }
 }
